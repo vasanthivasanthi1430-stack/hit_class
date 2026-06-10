68fddc710179c107edd70298021a5e7b816379ed
vasanthivasanthi1430-stack <vasanthivasanthi1430@gmail.com>
2026-06-10 23:11:05 +0530

    len.py

---
+++ len.py
@@ -0,0 +1,7 @@
+name=input("enter your name  : ")
+a=int(input("enter  a  value  is : "))
+b=int(input("enter   b value  is : "))
+sum=a+b
+print(f"hello {name}")
+print("sum is : ", sum)
+print("length  ",len(name))
